433a953df3f045861b7dd983665367656d54efc9
mitsui-h <65877209+mitsui-sys@users.noreply.github.com>
2023-09-06 10:14:20 +0900

    Update import.js

--- import.js
+++ import.js
@@ -1,4 +1,25 @@
 const txt = "Hello,World";
+const dataSet = [
+  {
+    title: "top",
+    uri: "/top",
+    description: "このwebサイトのTOPページ",
+    tags: ["website"],
+  },
+  {
+    title: "about",
+    uri: "/about",
+    description: "このwebサイトの説明",
+    tags: ["about"],
+  },
+  {
+    title: "blog",
+    uri: "/blog",
+    description: "ブログ一覧",
+    tags: ["article"],
+  }
+];
+
 const addControl = (map,control) => {
   map.addControl(control);
 };
@@ -7,3 +28,50 @@ export default {
   txt,
   addControl
 };
+
+const createElement = ({ tagName, children }) => {
+  const element = document.createElement(tagName);
+  if (children) {
+    element.appendChild(children);
+  }
+  return element;
+}
+
+const createFragment = ({ children }) => {
+  const fragment = document.createDocumentFragment()
+  children.forEach(child => {
+    fragment.appendChild(child);
+  });
+  return fragment;
+}
+
+const createMenuItem = (props) => {
+  const anchor = createElement({ tagName: "a" });  
+  anchor.text = props.anchor.text;
+  anchor.href = props.anchor.href;
+  return createElement({ tagName: "li", children: anchor });
+}
+
+const createMenu = (props) => {
+  const menuItems = props.items.map(createMenuItem);
+  return createElement({ tagName: "ul", children: createFragment(menuItems) });
+}
+
+const generateMenuProps = (dataSet, baseUrl) => {
+  const items = dataSet.map(param => {
+    return {
+      anchor: {
+        text: param.title,
+        href: baseUrl + param.uri,
+      },
+    };
+  });
+  return { items };
+}
+
+const menuProps = generateMenuProps(dataSet, "https://example.com");
+
+createMenu(menuProps);
+
+
+
