496b0e973ba2ca799a5c7b77557bcb561a76f249
mitsui-h <65877209+mitsui-sys@users.noreply.github.com>
2023-09-06 10:24:47 +0900

    Update import.js

--- import.js
+++ import.js
@@ -1,4 +1,4 @@
-const txt = "Hello,World";
+const helloworld = "Hello,World";
 const dataSet = [
   {
     title: "top",
@@ -19,15 +19,6 @@ const dataSet = [
     tags: ["article"],
   }
 ];
-
-const addControl = (map,control) => {
-  map.addControl(control);
-};
-
-export default {
-  txt,
-  addControl
-};
 
 const createElement = ({ tagName, children }) => {
   const element = document.createElement(tagName);
@@ -72,6 +63,54 @@ const generateMenuProps = (dataSet, baseUrl) => {
 const menuProps = generateMenuProps(dataSet, "https://example.com");
 
 createMenu(menuProps);
+
+function fetchUserInfo(userId) {
+    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error("エラーレスポンス", response);
+            } else {
+                return response.json().then(userInfo => {
+                    // HTMLの組み立て
+                    const view = escapeHTML`
+                    <h4>${userInfo.name} (@${userInfo.login})</h4>
+                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+                    <dl>
+                        <dt>Location</dt>
+                        <dd>${userInfo.location}</dd>
+                        <dt>Repositories</dt>
+                        <dd>${userInfo.public_repos}</dd>
+                    </dl>
+                    `;
+                    // HTMLの挿入
+                    const result = document.getElementById("result");
+                    result.innerHTML = view;
+                });
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+}
+
+function escapeSpecialChars(str) {
+    return str
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+function escapeHTML(strings, ...values) {
+    return strings.reduce((result, str, i) => {
+        const value = values[i - 1];
+        if (typeof value === "string") {
+            return result + escapeSpecialChars(value) + str;
+        } else {
+            return result + String(value) + str;
+        }
+    });
+}
 
 
 
